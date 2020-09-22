feebbc8908e4dd0d1ac6b68a8b21a85b1f54f343
chandanaCL <chandanacl462@gmail.com>
2020-09-22 19:04:35 +0530

    Application

---
+++ build/precache-manifest.96a1920792dab1bd61857418f75652f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0782986d3bf00a2d40343b9f3ab47ca3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52171e231759bcc7bdda",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "e85b64657a9c448f8a6f",
+    "url": "/static/css/main.e7e695d0.chunk.css"
+  },
+  {
+    "revision": "52171e231759bcc7bdda",
+    "url": "/static/js/2.1c92b707.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1c92b707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e85b64657a9c448f8a6f",
+    "url": "/static/js/main.16f80daa.chunk.js"
+  },
+  {
+    "revision": "0529a98c11a206755e39",
+    "url": "/static/js/runtime-main.99490353.js"
+  }
+]);
